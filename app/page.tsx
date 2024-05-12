b4fd5b36f7a15fddb938ecae6906bf9828e1909d
techiemokhele <neomokhele23@gmail.com>
2024-05-12 10:47:01 +0200

    Implement file structure & package install

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
-
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="text-white">Main</h1>
+    </div>
   );
 }
